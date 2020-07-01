be0fa71adf5ec7ca7f24a65ac030d849624717ad
unknown <mohamedkhaledkassab@gmail.com>
2020-07-01 18:12:08 +0200

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login')
+]
+
